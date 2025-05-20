f786987dc4d97d3726b05ec22b1775bc0e6430c0
Christian Escamilla <capimarquez23@gmail.com>
2025-05-20 15:27:38 -0600

    Revert "pwa funcionando"
    This reverts commit d9d2eb68739b1f2c47dc5c049a301a66709e31a7.

--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss(),
-        VitePWA({
-            registerType: "autoUpdate",
-            includeAssets: [
-                "vite.svg",
-                "images/logo.png",
-                "images/Fondo.png",
-                "images/inicio.webp",
-                "images/usr.webp",
-            ],
-            manifest: {
-                display: "standalone",
-                display_override: ["window-controls-overlay"],
-                lang: "es-ES",
-                name: "NotWasteTaste",
-                short_name: "NotWasteTaste",
-                description:
-                    "Aplicación para la gestión de desperdicios alimentarios",
-                theme_color: "#ffffff",
-                background_color: "#ffffff",
-                icons: [
-                    {
-                        src: "images/logo-64x64.png",
-                        sizes: "64x64",
-                        type: "image/png",
-                    },
-                    {
-                        src: "images/logo-192x192.png",
-                        sizes: "192x192",
-                        type: "image/png",
-                    },
-                    {
-                        src: "images/logo-512x512.png",
-                        sizes: "512x512",
-                        type: "image/png",
-                    },
-                ],
-            },
-        }),
-    ],
-});
+  plugins: [react(), tailwindcss()],
+})
